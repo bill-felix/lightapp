601bef375ed3de8d17295303214704c2282cec6e
Bill Felix <bill.felix@gmail.com>
2015-11-21 02:49:14 -0500

    instructions added and plots altered

--- server.R
+++ server.R
@@ -26,8 +26,9 @@ shinyServer(function(input, output) {
     tabl <- rbindlist(dat)
     
     
-    library(RColorBrewer)
+    
     # draw a plot showing changes occurring throughout consectutive runs per experiment
+    library(RColorBrewer)
     g <- ggplot(tabl, aes(x = Run, y = Speed)) + geom_point(shape = 1) + geom_smooth(method=lm) +
         xlim(c(1,max(morley$Run))) + facet_grid(. ~ Expt) + 
         xlab("Runs in each Block") + ylab("Speed of Light km/s (minus 299,000)")
@@ -38,20 +39,20 @@ shinyServer(function(input, output) {
         
         # use the slider input to determine how much data should be plotted
         run_number <- input$run
-        selected_runs <- 2:run_number
+        selected_runs <- 1:run_number
         
         # build the necessary dataframe and populate the empty list
-        for (n in selected_runs) {dat[[n-1]] = data.frame(morley) %>% filter(Run <= n) %>%
+        for (n in selected_runs) {dat[[n]] = data.frame(morley) %>% filter(Run < n + 1) %>%
             group_by(Expt) %>% 
             summarize(Run = max(Run)
                       , Speed = last(Speed)
-                      , sd = sd(Speed)
                       , mean = mean(Speed))}
         
         # stack the dataframes in the list
         library(data.table)
         tabl <- rbindlist(dat)
         
+        # plot the boxplots for each Block
         library(RColorBrewer)
         dd <- tabl %>% group_by(Expt) %>% ggplot() + 
             geom_boxplot(outlier.size = 4, aes(Run,Speed, fill = factor(Expt))) + 
@@ -59,26 +60,22 @@ shinyServer(function(input, output) {
             xlim(c(1,max(morley$Run))) + xlab("Runs in each Block") + ylab("Speed of Light km/s (minus 299,000)") +
             guides(fill=F)
         dd
+        
         # build a comparison table
         comp <- morley %>% group_by(Expt) %>% 
             summarize(max = max(Speed)
-                      , min = min(Speed)
-                      , mean = mean(Speed)
-                      , sd = sd(Speed))
-#         
-#         # draw a plot showing changes occurring throughout consectutive runs per experiment
-#         g <- ggplot(tabl, aes(x = Run, y = mean)) + geom_line()
-#         gg <- g + xlim(c(1,20)) + facet_grid(. ~ Expt)
-#         # gg <- g + geom_line(aes(color = factor(Expt))) + xlim(c(1,20)) + facet_grid(. ~ Expt)
-        hh <- dd + geom_hline(aes(yintercept = max), data = comp, color = "red", linetype = "longdash") + 
-            geom_hline(aes(yintercept = min), data = comp, color = "blue", linetype = "longdash") +
-            geom_hline(aes(yintercept = mean), data = comp, linetype = "dashed")
+                      , seventyfive = quantile(Speed, probs = .75)
+                      , mean = quantile(Speed, probs = .5)
+                      , twentyfive = quantile(Speed, probs = .25)
+                      , min = min(Speed))
+
+        # Add lines to compare to the changing boxplots
+        hh <- dd + geom_hline(aes(yintercept = max), data = comp, color = "dark red", linetype = "longdash") + 
+            geom_hline(aes(yintercept = min), data = comp, color = "dark blue", linetype = "longdash") +
+            geom_hline(aes(yintercept = seventyfive), data = comp, color = "red", linetype = "dashed") +
+            geom_hline(aes(yintercept = twentyfive), data = comp, color = "blue", linetype = "dashed") +
+            geom_hline(aes(yintercept = mean), data = comp, color = "purple", linetype = "dashed")
         hh
-        hh
-#         jj <- hh + xlab("Run") +
-#             ylab("Speed of Light (km/s minus 299,000)") +
-#             scale_colour_discrete(guide = F)
-#         jj
   })
 
 })
--- ui.R
+++ ui.R
@@ -1,41 +1,19 @@
+#lightapp ui
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
 
 library(shiny)
 shinyUI(fluidPage(verticalLayout(
-            titlePanel("Michelson Speed of Light Data"),
+            titlePanel("lightapp"),
+            mainPanel(h3("We have preloaded Michelson's Speed of Light data and you can explore his experiments by advancing the slider below:")),
+            mainPanel(h4("Notice how the Linear Model is adjusted as the experiment moves forward.")),
             plotOutput("lightPlot"),
             wellPanel(sliderInput("run", 
                                   "Consectutive Runs", 
                                   min = 2, 
                                   max = 20, 
                                   value = 2)),
+            mainPanel(h4("Take a look at the changing boxplots while seeing the experiment's true Max, 75th Quantile, Mean, 25th Quantile, & Min.")),
             plotOutput("aboutPlot")
             
         )
 ))
-# shinyUI(pageWithSidebar(
-#   
-#   # Application title
-#   headerPanel("Michelson Speed of Light Data"),
-#   
-#   # Sidebar with 
-#   sidebarPanel(
-#       selectInput("measure",
-#                   "Choose a Measure:",
-#                   choices = c("mean","sd","max","min","median")),
-#       sliderInput("run",
-#                   "Consectutive Runs",
-#                   min = 2,
-#                   max = 20,
-#                   value = 3)
-#   ),
-#   # Show a plot
-#   mainPanel(
-#     plotOutput("lightPlot")
-#   )
-# ))
